c9192fff6d1a5501e447eb6d423673cbda861da5
mario pontillo <mariopontillo6@gmail.com>
2022-08-01 12:31:28 +0200

    Adding Cloud-gateway

---
+++ paymentchain_parent/paymentchain_infrastructure/cloud-gateway/src/test/java/com/paymentchain/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentchain.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
